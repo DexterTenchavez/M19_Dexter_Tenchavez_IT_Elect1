bd51231b0a07d55ed16c24c22c03d61de6e91d40
Dexter Tenchavez <dextertenchavez@gmail.com>
2025-11-11 14:06:50 +0800

    adding files

--- App.js
+++ App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import CounterApp from './CounterApp';
 import ColorChangerApp from './ColorChangerApp';
+import ChatScreen from "./ChatScreen.js";
+import CommentInput from "./CommentInput.js";
 
 const App = () => {
+  const [ page,setPage]=useState("chat");
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <CounterApp />
         <ColorChangerApp />
       </ScrollView>
+      
+      {page === "chat" &&<ChatScreen/>}
+ {page === "comment"&&<CommentInput/>}
+  <View style={styles.buttons}>
+  <Button title="Go to Chat"
+  onPress={()=>setPage("chat")}/>
+  <Button title="Go to Comment"
+  onPress={()=>setPage("comment")}/>
+
+  
+  </View>
     </SafeAreaView>
   );
 };
